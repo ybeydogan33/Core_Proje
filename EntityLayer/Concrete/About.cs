0671da42effe315c11d5029722cd41a18e47c0f7
ybeydogan33 <y.beydogan33@gmail.com>
2025-12-20 02:34:45 +0300

    Initial Commit

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -9,7 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class About
     {
-        
+        [Key]
         public int AboutID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
