fdcbae2a3209aa3e3a4f4ba6b6dc9d9208350e90
ybeydogan33 <y.beydogan33@gmail.com>
2025-12-20 16:25:34 +0300

    Katmanlar Arası Referans İşlemi ve SQL Lite dönüşümü

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    internal class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+}
+}
