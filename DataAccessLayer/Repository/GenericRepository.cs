fb3aa3495a6a588e4d6fb8394d48a877459d305a
ybeydogan33 <y.beydogan33@hotmail.com>
2025-12-21 03:28:14 +0300

    Entity Framework Dal

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repository
 {
-    internal class GenericRepository<T> : IGenericDal<T> where T : class
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
         public void Delete(T t)
         {
