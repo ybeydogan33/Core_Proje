4f330af17611ae8d3160e8aa6daa2a6287394a84
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-02-23 16:37:42 +0300

    DbContext sınıfı ve bağlantı yapılandırması yapıldı.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer;
+
+public class Context : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=CoreProjeDB.db");
+    }
+}
