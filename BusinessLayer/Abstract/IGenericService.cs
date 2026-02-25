5d54f4d965b48f0cde4504c3ed17dc724bbe14d6
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-02-25 21:09:17 +0300

    Generic service tanımlandı.

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLayer;
+
+public interface IGenericService<T>
+{
+    void TAdd(T t);
+    void TDelete(T t);
+    void TUpdate(T t);
+    List<T> TGetList();
+    T GetByID(int id);
+
+}
