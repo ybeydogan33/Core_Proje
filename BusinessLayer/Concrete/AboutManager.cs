ccddacf65ed80c8aaf7a37659dc35df30e01b7f4
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-02-26 22:46:43 +0300

    Manager sınıfları tanımlandı

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer;
+
+namespace BusinessLayer;
+
+public class AboutManager : IAboutService
+{
+    public About GetByID(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TAdd(About t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TDelete(About t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<About> TGetList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TUpdate(About t)
+    {
+        throw new NotImplementedException();
+    }
+}
