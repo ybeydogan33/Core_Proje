fd0052fb4aab3c52073b68bac5375d60b6aa531f
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-01 17:40:32 +0300

    Tema dahil edildi.

---
+++ Core_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: DefaultController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
