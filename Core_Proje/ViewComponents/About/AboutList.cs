d3c67a88dde628c44368e22187d01559b857608a
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-01 21:11:53 +0300

    About List düzenlendi.

---
+++ Core_Proje/ViewComponents/About/AboutList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje;
+
+public class AboutList :ViewComponent
+{
+    AboutManager aboutManager = new AboutManager (new EfAboutDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = aboutManager.TGetList();
+        return View(values);
+    }   
+}
