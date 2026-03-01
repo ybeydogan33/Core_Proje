9ba96a5f9203a3b457c37293b897391154ec1ca2
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-01 21:40:46 +0300

    Service list düzenlendi.

---
+++ Core_Proje/ViewComponents/ServiceList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje;
+
+public class ServiceList:ViewComponent
+{
+    public ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = serviceManager.TGetList();
+        return View(values);
+    }
+}
