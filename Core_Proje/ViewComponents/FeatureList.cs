e2887adc8626100d22875d79f4c4dee6ef394928
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-01 20:04:38 +0300

    Feature list düzenlendi.

---
+++ Core_Proje/ViewComponents/FeatureList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje;
+
+public class FeatureList: ViewComponent
+{
+    FeatureManager featureManager= new FeatureManager(new EfFeatureDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = featureManager.TGetList();
+        return View(values);
+    }
+
+}
