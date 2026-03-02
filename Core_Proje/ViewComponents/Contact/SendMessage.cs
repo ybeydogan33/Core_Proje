54004c5b9d0a672334824279fb275bc1cb325bb3
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-02 22:36:22 +0300

    SendMessage düzenlendi.

--- Core_Proje/ViewComponents/Contact/SendMessage.cs
+++ Core_Proje/ViewComponents/Contact/SendMessage.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer;
 using DataAccessLayer;
+using EntityLayer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje;
@@ -7,9 +8,17 @@ namespace Core_Proje;
 public class SendMessage :ViewComponent
 {
     MessageManager messageManager = new MessageManager (new EfMessageDal());
+    [HttpGet]
     public IViewComponentResult Invoke()
     {
-        var values = messageManager.TGetList();
-        return View(values);
+        return View();
     }   
+    // [HttpPost]
+    // public IViewComponentResult Invoke(Message p)
+    // {
+    //     p.Date= Convert.ToDateTime(DateTime.Now.ToShortDateString());
+    //     p.Status=true;
+    //     messageManager.TAdd(p);
+    //     return View ();
+    //}
 }
