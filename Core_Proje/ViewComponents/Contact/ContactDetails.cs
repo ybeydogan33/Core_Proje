157fe62fa1d048a5ffcf41f1d80fad7fb1f991c2
Yavuz Beydogan <y.beydogan33@gmail.com>
2026-03-02 21:16:07 +0300

    ContactDetails list düzenlendi.

---
+++ Core_Proje/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje;
+
+public class ContactDetails :ViewComponent
+{
+    ContactManager contactManager = new ContactManager (new EfContactDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = contactManager.TGetList();
+        return View(values);
+    }   
+}
---
+++ Core_Proje/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje;
+
+public class SendMessage :ViewComponent
+{
+    MessageManager messageManager = new MessageManager (new EfMessageDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = messageManager.TGetList();
+        return View(values);
+    }   
+}
